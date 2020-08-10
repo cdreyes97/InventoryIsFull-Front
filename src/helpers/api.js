2dbe923aa659698f6c4b9b9945d7d93a339174ba
cdreyes <cdreyes@alumnos.inf.utfsm.cl>
2020-08-10 18:46:27 -0400

    link backend

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const endpoints = {
-    //development: 'https://inventory-is-full-back.herokuapp.com/',
-    development: 'http://127.0.0.1:9000',
+    development: 'https://inventory-is-full-back.herokuapp.com/',
+    //development: 'http://127.0.0.1:9000',
 };
 
 export const
